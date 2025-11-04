74bedbfaf760e6e46f77883895edc4bf3157acf8
M7MDB <m7mdbox1@gmail.com>
2025-11-04 15:12:01 +0300

    feat: Implement doctor's prescription view page
    Add a new page for doctors to view patient prescriptions.
    
    - Create `DoctorViewPrescriptionsPage` fragment and its corresponding layout file.
    - Implement back arrow functionality on both the new prescription page and the schedule view page.
    - Add a clickable "view" `TextView` on the `DoctorViewScheduleFragment` to navigate to the `DoctorViewPrescriptionsPage`.

---
+++ app/src/main/java/com/example/meditrackproject/DoctorViewPrescriptionsPage.java
@@ -0,0 +1,44 @@
+package com.example.meditrackproject;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+
+public class DoctorViewPrescriptionsPage extends Fragment {
+
+    ImageView arrowBack;
+
+    public DoctorViewPrescriptionsPage() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_doctor_view_prescriptions_page, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        arrowBack = view.findViewById(R.id.arrowBackForCurrentPage);
+
+        arrowBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requireActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/meditrackproject/DoctorViewScheduleFragment.java
+++ app/src/main/java/com/example/meditrackproject/DoctorViewScheduleFragment.java
@@ -14,7 +14,7 @@ import android.widget.TextView;
 
 public class DoctorViewScheduleFragment extends Fragment {
 
-    TextView patientEmailTextView, patientNameTextView;
+    TextView patientEmailTextView, patientNameTextView, viewPrescriptionsTextView;
     ImageView arrowBack;
 
 
@@ -36,12 +36,28 @@ public class DoctorViewScheduleFragment extends Fragment {
         arrowBack = view.findViewById(R.id.arrowBackForCurrentPage);
         patientEmailTextView = view.findViewById(R.id.patientEmailTextView);
         patientNameTextView = view.findViewById(R.id.patientNameTextView);
+        viewPrescriptionsTextView = view.findViewById(R.id.viewPrescriptionsTextView);
+
+        arrowBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requireActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
 
         String patientName = getArguments().getString("patientFullName");
         String patientEmail = getArguments().getString("patientEmail");
 
         patientNameTextView.setText(patientName);
         patientEmailTextView.setText(patientEmail);
+
+        viewPrescriptionsTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment nextFragment = new DoctorViewPrescriptionsPage();
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.doctor_fragment_container, nextFragment).addToBackStack(null).commit();
+            }
+        });
 
 
     }
