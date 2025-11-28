dad478f7ec17c643073063bbba81218b533b33a4
M7MDB <m7mdbox1@gmail.com>
2025-11-28 13:56:42 +0300

    Add back navigation logic to PatientNotificationPageFragment
    Implement `onViewCreated` to initialize the back arrow ImageView.
    Add a click listener to the back arrow that pops the back stack, allowing the user to navigate back to the previous screen.

--- app/src/main/java/com/example/meditrackproject/PatientNotificationPageFragment.java
+++ app/src/main/java/com/example/meditrackproject/PatientNotificationPageFragment.java
@@ -2,14 +2,18 @@ package com.example.meditrackproject;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class PatientNotificationPageFragment extends Fragment {
 
+    ImageView arrowBackImageView;
 
     public PatientNotificationPageFragment() {
         // Required empty public constructor
@@ -20,5 +24,18 @@ public class PatientNotificationPageFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_patient_notification_page, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        
+        arrowBackImageView = view.findViewById(R.id.arrowBackForPatientPage);
+        
+        arrowBackImageView.setOnClickListener(v -> arrowBackClicked());
+    }
+
+    private void arrowBackClicked() {
+        requireActivity().getSupportFragmentManager().popBackStack();
     }
 }
